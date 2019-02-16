5131dbccc03d3e2333c0bc903f3691aff9f1fdaf
arlinadsign <47700600+arlinadsign@users.noreply.github.com>
2019-02-17 04:31:01 +0800

    Add files via upload

---
+++ navcoment.js
@@ -0,0 +1,6 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function () { if ($("#change,.change").attr("href") != "https://xredesign.blogspot.com/") {
+        window.location.href = "https://xredesign.blogspot.com"; }});
+//]]>
+</script>
